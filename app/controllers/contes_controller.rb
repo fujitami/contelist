d5c4bf01c833583535fb37a2574ac00c4e695787
fujitami <midori.fujita.0423@gmail.com>
2025-02-23 02:42:49 +0900

    新規登録機能の追加

--- app/controllers/contes_controller.rb
+++ app/controllers/contes_controller.rb
@@ -1,9 +1,26 @@
 class ContesController < ApplicationController
   def index
-    @contes = Conte.all
+    @contes = Conte.all.order(created_at: :desc)
+  end
+
+  def new
+    @conte = Conte.new
+  end
+
+  def create
+    if Conte.create(conte_params)
+      redirect_to contes_path
+    else
+      render 'new'
+    end
   end
 
   def show
-    @conte = Conte.find_by(params[:id])
+    @conte = Conte.find_by(id: params[:id])
+  end
+
+  private
+  def conte_params
+    params.require(:conte).permit(:title, :people, :trigger, :props_costumes, :debut)
   end
 end
